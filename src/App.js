d2345b7b2c72b806ec1e65b0f4e90a027ea924e9
Beaune <mathis.beaune13@gmail.com>
2021-04-07 14:30:47 +0200

    Deuxieme commit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,29 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
-      firstName : "John"
+      firstName : "John",
+      nomValue : "",
+      prenomValue : "",
+      userAlert : alert('Bonjour ' + this.state.nomValue + ' ' + this.state.prenomValue)
     }
   }
   render() {
-    console.log(this.state.firstName);
     return (
       <div className="App">
       <header className="App-header">
         <p>Welcome { this.state.firstName  } </p>
+        <form>
+          Nom:
+          <label>
+            <input type="text" name="Nom" Value={this.state.nomValue}
+             />
+          </label>
+          Prénom:
+          <label>
+            <input type="text" name="Prenom" Value={this.state.prenomValue} />
+          </label>
+          <input type="submit" value="Envoyer" onClick={() => this.state.userAlert } />
+        </form>
       </header>
     </div>
     );
